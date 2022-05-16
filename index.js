aa23d81fd941b9bd8e85914860fb98fa1cacd34e
ivan <example@gmail.com>
2022-05-16 13:47:15 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// const obj = {
+//     name: "Tanya",
+//     age: 27,
+//     isMarried: true
+// };
+
+// console.log(obj.name);
+
+
+// let arr = ["Tanya", "Andrey", "Baton"];
+// console.log(arr[1]);
+
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt("Как Ваше имя?", "");
+// answers[1] = prompt("Как Ваша фамилия?", ""); 
+// answers[2] = prompt("Сколько Вам лет?", "");
+// console.log(answers[0]);
+// console.log(answers[1]);
+// console.log(answers[2]); 
+// console.log(typeof(answers));
+
+
+// const category = "toys";
+// console.log(`http://someurl.com/${category}/5`);
+
+
+// const user = "Tatiana";
+// alert(`Привет, ${user}`);
+
+
+console.log(2+2*2 === 6);
